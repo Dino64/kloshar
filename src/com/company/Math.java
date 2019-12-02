9fc131ae24e391bad963e9fb9c40a6503a5c2662
Dino64 <dino.dsu@gmail.com>
2019-12-02 17:03:24 +0100

    Added Add method to the Math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,4 +1,21 @@
 package com.company;
 
 public class Math {
+    public static void main(String[] args) {
+
+Math myApp = new Math();
+    int a = 2;
+    int b = 4;
+    int add = myApp.add(a, b);
+
+        System.out.println(add);
+
+    }
+
+    public int add (int first, int second){
+        int sum = 0;
+        sum = first + second;
+
+        return sum;
+    }
 }
