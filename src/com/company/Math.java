54fba9f4e965cc971d67d780a88e386d03124e4c
Dino64 <dino.dsu@gmail.com>
2019-12-02 17:15:12 +0100

    Added sub method to the Math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -7,8 +7,12 @@ Math myApp = new Math();
     int a = 2;
     int b = 4;
     int add = myApp.add(a, b);
-
         System.out.println(add);
+        int one = 5;
+        int two = 11;
+
+        int sub = myApp.sub(one,two);
+        System.out.println(sub);
 
     }
 
@@ -16,6 +20,12 @@ Math myApp = new Math();
         int sum = 0;
         sum = first + second;
 
+        return sum;
+    }
+
+    public int sub(int first, int second){
+        int sum = 0;
+        sum = first - second;
         return sum;
     }
 }
